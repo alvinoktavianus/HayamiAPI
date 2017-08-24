37c630d235f0751bbbc6fc0e9d230506fcfb5b04
Alvin Oktavianus <agentsam1995@gmail.com>
2017-08-24 22:38:04 +0700

    Initialize migration script

---
+++ HayamiAPI/Migrations/201708241536065_InitialCreate.cs
@@ -0,0 +1,31 @@
+namespace HayamiAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserID = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        UserPassword = c.String(),
+                        UserRole = c.String(),
+                        UserImg = c.String(),
+                        CreatedAt = c.DateTime(nullable: false),
+                        UpdDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
