95a2a990b3c1b7b4c8e378c41de5e49e16f0c2cb
Alvin Oktavianus <agentsam1995@gmail.com>
2017-08-24 22:37:17 +0700

    Scaffold user controller

---
+++ HayamiAPI/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HayamiAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string UserRole { get; set; }
+        public string UserImg { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdDate { get; set; }
+    }
+}
