f48246ea4d0e220fc4d75a913a40dc36fea761c9
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-26 11:21:35 +0700

    AddCounterModel

---
+++ HayamiAPI/Models/Counter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HayamiAPI.Models
+{
+    public class Counter
+    {
+        public int CounterID { get; set; }
+        public string CounterName { get; set; }
+        public string CounterAddr { get; set; }
+        public string CounterCity { get; set; }
+        public string CounterPosCode { get; set; }
+        public string CounterPhone { get; set; }
+        public string CounterEmail { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdDate { get; set; }
+        public string UpdatedBy { get; set; }
+        
+    }
+}
