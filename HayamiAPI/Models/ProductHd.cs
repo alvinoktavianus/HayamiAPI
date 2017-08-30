34500877ea2ff5a09b370a9c8e590a9b9b8d8c2a
Michael Wijaya <michaelwijaya_1996@yahoo.com>
2017-08-30 20:19:35 +0700

    Add ProductHd Model

---
+++ HayamiAPI/Models/ProductHd.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HayamiAPI.Models
+{
+    public class ProductHd
+    {
+        public int ProductHdID { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDesc { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public int TypeID { get; set; }
+        public int ModelID { get; set; }
+
+        // Relationship definition
+        [ForeignKey("TypeID")]
+        public Type Type { get; set; }
+        [ForeignKey("ModelID")]
+        public Model Model { get; set; }
+        
+    }
+}
